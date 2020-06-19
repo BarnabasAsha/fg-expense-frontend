c9fc0652f02fcc4f11c66b27957ac28b65db47dc
Barnabas Asha <barnabee58@gmail.com>
2020-06-19 22:19:05 +0100

    chat

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ $(document).ready(function(){
         $('.nav ul').toggleClass('showing');
     })
 })
+
+document.querySelector('.edit').addEventListener('click', () => {
+   const chatBox = document.querySelector('.chat-box');
+   const options = document.createElement('div');
+   options.className = 'option'
+   options.innerHTML = `
+   <ul>
+   <li><a href="">Edit</a></li>
+   <li><a href="">Delete</a></li>
+   </ul>
+   `
+   chatBox.appendChild(options)
+   options.style.display = 'block'
+   
+   
+})
